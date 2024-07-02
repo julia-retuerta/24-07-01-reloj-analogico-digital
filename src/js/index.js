ba6655d1c6fed3318cc143e504b644923ce6c8f9
juliaret <julia.retuertanogales@alumnostrazos.net>
2024-07-02 15:47:26 +0200

    practica-casa-02-julio

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,13 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+// Digital clock
+
+const digitalClockHoursElement = document.getElementById('digital-clock-hours');
+const digitalClockMinutesElement = document.getElementById('digital-clock-minutes');
+const digitalClockWeekDayElement = document.getElementById('digital-clock-week-day');
+const digitalClockDayElement = document.getElementById('digital-clock-day');
+const digitalClockMonthElement = document.getElementById('digital-clock-month');
+const digitalClockYearElement = document.getElementById('digital-clock-year');
+
+setIntervalHour(() => {}, 3600000);
